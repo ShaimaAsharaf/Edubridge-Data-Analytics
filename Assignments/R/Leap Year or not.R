058c68444fd09f2f09ccffbab574e7972c69c477
ShaimaAsharaf <98824145+ShaimaAsharaf@users.noreply.github.com>
2022-03-29 18:08:31 +0530

    initial commit

---
+++ Assignments/R/Leap Year or not.R
@@ -0,0 +1,15 @@
+year<-readline(prompt="enter the year")
+year<-as.integer(year)
+if((year %% 4) == 0) {
+  if((year %% 100) == 0) {
+    if((year %% 400) == 0) {
+      print(paste(year,"is a leap year"))
+    } else {
+      print(paste(year,"is not a leap year"))
+    }
+  } else {
+    print(paste(year,"is a leap year"))
+  }
+} else {
+  print(paste(year,"is not a leap year"))
+}
